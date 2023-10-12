bd081cddd60fad148ccd752ca63b8754033c4ee5
Kenneth2Shu <darkryder42@gmail.com>
2023-10-12 18:45:32 +0800

    updated scoring

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -11,16 +11,26 @@ public class ScoreSystem : MonoBehaviour
     [SerializeField] private TMP_Text scoreTextValue;
     [SerializeField] private Animator animator;
 
+    private bool bPlaying;
+
     // Start is called before the first frame update
     void Start()
     {
         fScore = 0;
+        bPlaying = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        fScore = Mathf.Abs(animator.GetFloat("Speed"));
+        while(bPlaying == true) {
+            fScore = Mathf.Abs(animator.GetFloat("Speed"));
+        }
         scoreTextValue.text = fScore.ToString();
+    }
+
+    //call this when pausing or ending the level
+    public void StopScoring() {
+        bPlaying = false;
     }
 }
